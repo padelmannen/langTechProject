01920973cedb1e5bb18da7d76e70fdf3a5632534
padelmannen <jesper.senke@gmail.com>
2021-12-13 20:22:43 +0100

    fungerande tränare och sparad  modell. Både för kategori och okatergori

--- CSVformatter.py
+++ CSVformatter.py
@@ -3,10 +3,25 @@ import csv
 filename = 'winemag-data-130k-v2.csv'
 
 with open(filename, 'r', encoding="utf8") as csvfile:
-    w = csv.writer(open("XYdata.csv", "w", encoding="utf8"))
+    w = csv.writer(open("catXYdata.csv", "w", encoding="utf8"))
     datareader = csv.reader(csvfile)
+
     for row in datareader:
-        w.writerow([row[2], row[4]])
+        if row[4]=="points":
+            rate = "points"
+        else:
+            rate = int(row[4])
+            if 80 <= rate < 85:
+                rate = str("OK")
+            elif 85 <= rate < 90:
+                rate = str("Good")
+            elif 90 <= rate < 95:
+                rate = str("Tasty")
+            else:
+                rate = str("Perfect")
+
+        w.writerow([row[2], rate])
+
 
 
 
--- Trainer.py
+++ Trainer.py
@@ -1,22 +1,35 @@
+import os
 import pickle
 
 import numpy as np
 import pandas as pd
+from sklearn import metrics
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 
-data = pd.read_csv('CleanXYdata.csv')
+# modelPath = "./model.sav"     ##Om vi vill köra utan kategorier
+# dataSet = "CleanXYdata.csv"
+# targets = ["80", "90", "100"]
+
+modelPath = "./modelCat.sav"      ##Om vi vill köra med kategorier
+dataSet = "CleanCatXYdata.csv"
+targets = ["OK", "Good", "Tasty", "Perfect"]
+
+data = pd.read_csv(dataSet)
 X_train, X_test, y_train, y_test = train_test_split(data.cleanedDesc, data.points, test_size=0.2)
 
-pipeline = Pipeline([('vect', TfidfVectorizer(ngram_range=(1, 2), stop_words="english", sublinear_tf=True)),
-                     ('chi', SelectKBest(chi2, k=10000)),
-                     ('clf', LinearSVC(C=1.0, penalty='l1', max_iter=3000, dual=False))])
+if not (os.path.exists(modelPath)):  #if model dont exist in dir
+    pipeline = Pipeline([('vect', TfidfVectorizer(ngram_range=(1, 2), stop_words="english", sublinear_tf=True)),
+                         ('chi', SelectKBest(chi2, k=100000)),
+                         ('clf', LinearSVC(C=1.0, penalty='l1', max_iter=3000, dual=False))])
 
-model = pipeline.fit(X_train, y_train)
-pickle.dump(model, open('model.sav', 'wb'))
+    model = pipeline.fit(X_train, y_train)
+    pickle.dump(model, open(modelPath, 'wb'))
+else:
+    model = pickle.load(open(modelPath, 'rb'))
 
 vectorizer = model.named_steps['vect']
 chi = model.named_steps['chi']
@@ -25,17 +38,14 @@ clf = model.named_steps['clf']
 featureNames = vectorizer.get_feature_names()
 featureNames = [featureNames[i] for i in chi.get_support(indices=True)]
 featureNames = np.asarray(featureNames)
+predicted = model.predict(X_test)
 
-targets = ["80", "90", "100"]
 for i, label in enumerate(targets):
     top10 = np.argsort(clf.coef_[i])[-10:]
     print("%s: %s" % (label, " ".join(featureNames[top10])))
 
-
-# some time later...
-
-# load the model from disk
-loaded_model = pickle.load(open('model.sav', 'rb'))
-result = loaded_model.score(X_test, y_test)
+result = model.score(X_test, y_test)
+print(metrics.classification_report(y_true=y_test, y_pred=predicted))
 print(result)
-
+print(model.predict(["that was not so tasteful, but i liked the sweetness"]))
+print(model.predict(["that was probably the best wine i have ever tasted, fantastic. Round taste"]))
