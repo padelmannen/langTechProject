159b71f7fa4f355293d719dd38c77b6f9b387ced
padelmannen <jesper.senke@gmail.com>
2021-12-13 15:16:45 +0100

    skelett och cleanat dataset

--- DataCleaner.py
+++ DataCleaner.py
@@ -4,9 +4,11 @@ import pandas as pd
 from nltk import SnowballStemmer
 from nltk.corpus import stopwords
 
+##Tvättningen av datasetet gör: 1) om alla ord till sin grundform m.h.a. stemmer. 2) Tar bort vanligt förekommande ord från stopwords, ex. "for", "and" och "i". 3) Gör om alla bokstäver till små.
+
 data = pd.read_csv('XYdata.csv')
 print(data)
-stemmer = SnowballStemmer('english')
-stopWords = stopwords.words('english')
+stemmer = SnowballStemmer('english')    ##gör om ord till sin grundform
+stopWords = stopwords.words('english')  ##vanligt förekommande ord
 data['cleanedDesc'] = data['description'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z)]", " ", x).split() if i not in stopWords]).lower())
 data[['cleanedDesc', 'points']].to_csv('CleanXYdata.csv')
