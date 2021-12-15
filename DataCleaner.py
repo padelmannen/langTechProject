22d02c5a3f66f2b4b04c0079e804213bff987fcd
padelmannen <91955796+padelmannen@users.noreply.github.com>
2021-12-15 15:35:11 +0100

    Add files via upload

--- DataCleaner.py
+++ DataCleaner.py
@@ -1,14 +1,14 @@
 import re
+import string
 
 import pandas as pd
 from nltk import SnowballStemmer
 from nltk.corpus import stopwords
 
-##Tvättningen av datasetet gör: 1) om alla ord till sin grundform m.h.a. stemmer. 2) Tar bort vanligt förekommande ord från stopwords, ex. "for", "and" och "i". 3) Gör om alla bokstäver till små.
+##Tvättnngen av datasetet gör: 1) Tar bort alla skiljetecken 2) Tar bort vanligt förekommande ord från stopwords, ex. "for", "and" och "i". 3) Gör om alla bokstäver till små.
 
 data = pd.read_csv('catXYdata.csv')
-print(data)
 stemmer = SnowballStemmer('english')    ##gör om ord till sin grundform
 stopWords = stopwords.words('english')  ##vanligt förekommande ord
-data['cleanedDesc'] = data['description'].apply(lambda x: " ".join([stemmer.stem(i) for i in re.sub("[^a-zA-Z)]", " ", x).split() if i not in stopWords]).lower())
-data[['cleanedDesc', 'points']].to_csv('CleanCatXYdata.csv')
+data['cleanedDesc'] = data['description'].apply(lambda x: " ".join([i for i in re.sub("[^a-zA-Z)]", " ", x).split() if i not in stopWords]).lower())
+data[['cleanedDesc', 'points']].to_csv('CleanXYdata.csv')
--- Trainer.py
+++ Trainer.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-
 import numpy as np
 import pandas as pd
 from sklearn import metrics
@@ -9,24 +8,38 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
+#from sklearn.svm import LinearSVC
 
-# modelPath = "./model.sav"     ##Om vi vill köra utan kategorier
-# dataSet = "CleanXYdata.csv"
-# targets = ["80", "90", "100"]
+dataSet = "CleanXYdata.csv"
 
-modelPath = "./NBmodelCat.sav"      ##Om vi vill köra med kategorier
-dataSet = "CleanCatXYdata.csv"
+
+algortim = MultinomialNB()
+#algoritm = LinearSVC()
+
+
+#nGram_range = (1, 1)
+#nGram_range = (1, 2)
+nGram_range = (1, 3)
+
+#modelPath = "oneGramNBmodel.sav"
+#modelPath = "twoGramNBmodel.sav"
+#modelPath = "threeGramNBmodel.sav"
+
+#modelPath = "oneGramLinModel.sav"
+#modelPath = "twoGramLinModel.sav"
+modelPath = "threeGramLinModel.sav"
+
+
+
 targets = ["OK", "Good", "Tasty", "Perfect"]
 
 data = pd.read_csv(dataSet)
 X_train, X_test, y_train, y_test = train_test_split(data.cleanedDesc, data.points, test_size=0.2)
 
 if not (os.path.exists(modelPath)):  #if model dont exist in dir
-    pipeline = Pipeline([('vect', TfidfVectorizer(ngram_range=(1, 2), stop_words="english", sublinear_tf=True)),
-                         ('chi', SelectKBest(chi2, k=100000)),
-                        # ('clf', LinearSVC(C=1.0, penalty='l1', max_iter=3000, dual=False))])
-                         ('clf', MultinomialNB())])
+    pipeline = Pipeline([('vect', TfidfVectorizer(ngram_range=nGram_range)),
+                         ('chi', SelectKBest(chi2, k=10000)),
+                         ('clf', algortim)])
 
     model = pipeline.fit(X_train, y_train)
     pickle.dump(model, open(modelPath, 'wb'))
@@ -52,3 +65,4 @@ print(metrics.classification_report(y_true=y_test, y_pred=predicted))
 print(result)
 print(model.predict(["that was not so tasteful, but i liked the sweetness"]))
 print(model.predict(["that was probably the best wine i have ever tasted, fantastic. Round taste"]))
+print(model.predict(["delicious wine, a deep complex taste with impressive deepness. Excellent work from the winemaker"]))
